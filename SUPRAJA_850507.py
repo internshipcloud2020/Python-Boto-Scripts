6c3fc47de76d21be205e98fae42b90e4852d1dd2
supraja29 <51958577+supraja29@users.noreply.github.com>
2020-03-18 14:08:50 +0530

    Add files via upload

---
+++ SUPRAJA_850507.py
@@ -0,0 +1,33 @@
+#To use the API, establish a connection using a personal access token and the URL to your Azure DevOps organization. Then get a client from the connection and make API calls.
+
+
+
+
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+import pprint
+
+# Fill in with your personal access token and org URL
+personal_access_token = 'YOURPAT'
+organization_url = 'https://dev.azure.com/YOURORG'
+
+# Create a connection to the org
+credentials = BasicAuthentication('', personal_access_token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+# Get a client (the "core" client provides access to projects, teams, etc)
+core_client = connection.clients.get_core_client()
+
+# Get the first page of projects
+get_projects_response = core_client.get_projects()
+index = 0
+while get_projects_response is not None:
+    for project in get_projects_response.value:
+        pprint.pprint("[" + str(index) + "] " + project.name)
+        index += 1
+    if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
+        # Get the next page of projects
+        get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
+    else:
+        # All projects have been retrieved
+        get_projects_response = None
